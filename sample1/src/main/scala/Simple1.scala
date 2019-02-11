bbc9d1c293e58251de8f9da1bc1dae5b812b732d
zerualem <zerualem@node-0.zerihun-assign1.michigan-bigdata-pg0.utah.cloudlab.us>
2019-02-11 00:04:46 -0700

    First commit

---
+++ sample1/src/main/scala/Simple1.scala
@@ -0,0 +1,14 @@
+/* Simple1.scala */
+import org.apache.spark.sql.SparkSession
+
+object Simple1 {
+  def main(args: Array[String]) {
+    val logFile = "sample.txt" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
